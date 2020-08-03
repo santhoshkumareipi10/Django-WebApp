9f10f6bc1d7782e4ac826f30832d5a897af1d0da
Santhosh Kumar <santhoshsanthu2k@gmail.com>
2020-08-03 15:22:14 +0530

    API Check

--- products/models.py
+++ products/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     image = models.ImageField(default='/default.jpeg', upload_to='profile_pics')
-    username = models.CharField(max_length=200, null=True)
     pet = models.CharField(max_length=20, null=True)
     pet_image = models.ImageField(default="/default_pet.png", upload_to='profile_pics')
     pet_name = models.CharField(max_length=20, null=True)
@@ -18,7 +17,7 @@ class Profile(models.Model):
     email = models.CharField(max_length=50, null=True)
 
     def __str__(self):
-        return f'{self.user.name} Profile'
+        return f'{self.user.username} Profile'
 
 
 class Signup(models.Model):
